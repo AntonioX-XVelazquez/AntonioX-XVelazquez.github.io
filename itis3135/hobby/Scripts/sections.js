afa04bdc7a1c97df71a0f537ca4f4e901c8f21fd
AntonioX-XVelazquez <avelazq8@charlotte.edu>
2025-03-18 05:24:10 -0400

    Should fix it hopefully

--- itis3135/hobby/Scripts/sections.js
+++ itis3135/hobby/Scripts/sections.js
@@ -1,10 +1,14 @@
 function toggleSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section.style.display === 'none' || section.classList.contains('hidden')) {
-      section.style.display = 'block';  
-      section.classList.remove('hidden');
-    } else {
-      section.style.display = 'none';  
+
+    const sections = document.querySelectorAll('div[id^="section"]');
+    sections.forEach(section => {
       section.classList.add('hidden');
-    }
+    });
+
+    const sectionToShow = document.getElementById(sectionId);
+    sectionToShow.classList.remove('hidden');
   }
+
+  window.onload = function() {
+    toggleSection('section1');
+  };
